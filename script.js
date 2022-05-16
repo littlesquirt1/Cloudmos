3344a8e04f6510e3ac1ccfe72f58992d428d03f1
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-05-16 16:36:13 +0000

    removed notes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var elt = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elt)
-var tmpState = calculator.getState();
+// var tmpState = calculator.getState();
 var cloudvars = calculator.HelperExpression({ latex: 'c_{loudvars}' });
 var socket = io();
 
-tmpState.expressions.list[0] = ({ id: '1', type: 'text', text: 'Welcome to Cloudmos!' });
-tmpState.expressions.list[1] = ({ id: '2', type: 'text', text: 'To start, click the button in the top-right to add cloud variables!' });
-tmpState.expressions.list[2] = ({ id: '3', type: 'text', text: 'This will create an array that is on the cloud!' });
-calculator.setState(tmpState);
+// tmpState.expressions.list[0] = ({ id: '1', type: 'text', text: 'Welcome to Cloudmos!' });
+// tmpState.expressions.list[1] = ({ id: '2', type: 'text', text: 'To start, click the button in the top-right to add cloud variables!' });
+// tmpState.expressions.list[2] = ({ id: '3', type: 'text', text: 'This will create an array that is on the cloud!' });
+// calculator.setState(tmpState);
 
 function addCloud() {
   calculator.setExpression({ id: 'cloud', latex: 'c_{loudvars}=[]' });
