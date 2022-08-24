00490691a587bfd1b515ff66ddf8272319d890d9
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-08-24 14:12:42 +0000

    added a lot

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ var calculator = Desmos.GraphingCalculator(elt)
 // var tmpState = calculator.getState();
 var cloudvars = calculator.HelperExpression({ latex: 'c_{loudvars}' });
 var socket = io();
+addCloud();
 
 // tmpState.expressions.list[0] = ({ id: '1', type: 'text', text: 'Welcome to Cloudmos!' });
 // tmpState.expressions.list[1] = ({ id: '2', type: 'text', text: 'To start, click the button in the top-right to add cloud variables!' });
 // tmpState.expressions.list[2] = ({ id: '3', type: 'text', text: 'This will create an array that is on the cloud!' });
 // calculator.setState(tmpState);
 
+// function popup() {
+//   document.getElementById("popup-bg").style = "display: inline;"
+//   document.getElementById("popup").style = "display: inline;"
+// }
+
+// function close() {
+//   document.getElementById("popup-bg").style.display = 'none'
+//   document.getElementById("popup").style.display = 'none'
+// }
+
 function addCloud() {
-  calculator.setExpression({ id: 'cloud', latex: 'c_{loudvars}=[]' });
+  if (!(calculator.getState().expressions.list.find((x) => x.id === 'cloud' && x.latex.substr(0,13) === "c_{loudvars}="))) {
+    calculator.setExpression({ id: 'cloud', latex: 'c_{loudvars}=[]' });
+  }
 }
 
 
@@ -24,3 +37,4 @@ socket.on('cloudvar', function(msg) {
   }
   
 });
+
